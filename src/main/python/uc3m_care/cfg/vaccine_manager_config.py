0ce50d101edd016e0e5d2d1a911d5f3c76d915f9
adelpozoman <adelpozoman@gmail.com>
2022-05-13 11:18:35 +0200

    refix path

--- src/main/python/uc3m_care/cfg/vaccine_manager_config.py
+++ src/main/python/uc3m_care/cfg/vaccine_manager_config.py
@@ -1,4 +1,4 @@
 """Global constants for finding the path"""
 from pathlib import Path
-JSON_FILES_PATH = str(Path.home()) + "/PycharmProjects/G81.2022.14.FP/src/JsonFiles"
+JSON_FILES_PATH = str(Path.home()) + "/PycharmProjects/G81.2022.14.FP/src/JsonFiles/"
 JSON_FILES_RF2_PATH = JSON_FILES_PATH + "/RF2/"
