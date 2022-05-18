88d1578aaf8f31ade806616917ae937029b19e8f
adelpozoman <adelpozoman@gmail.com>
2022-05-18 22:54:21 +0200

    all pylint and typing errors

--- src/main/python/uc3m_care/storage/json_store.py
+++ src/main/python/uc3m_care/storage/json_store.py
@@ -5,16 +5,18 @@ import os
 
 from uc3m_care.exception.vaccine_management_exception import VaccineManagementException
 
-class JsonStore():
+
+class JsonStore:
     """Superclass for managing storage in JSON files"""
     _FILE_PATH = ""
     _ID_FIELD = ""
     _data_list = []
-    def __init__( self ):
+
+    def __init__(self):
         self.load()
 
-    def load( self ):
-        """Loading data into the datalist"""
+    def load(self):
+        """Loading data into the data-list"""
         try:
             with open(self._FILE_PATH, "r", encoding="utf-8", newline="") as file:
                 self._data_list = json.load(file)
@@ -25,22 +27,22 @@ class JsonStore():
             raise VaccineManagementException("JSON Decode Error - Wrong JSON Format") \
                 from exception_raised
 
-    def save( self ):
-        """Saves the datalist in the JSON file"""
+    def save(self):
+        """Saves the data-list in the JSON file"""
         try:
             with open(self._FILE_PATH, "w", encoding="utf-8", newline="") as file:
                 json.dump(self._data_list, file, indent=2)
         except FileNotFoundError as ex:
             raise VaccineManagementException("Wrong file or file path") from ex
 
-    def add_item( self, item ):
-        """Adds a new item to the datalist and updates the JSON file"""
+    def add_item(self, item):
+        """Adds a new item to the data-list and updates the JSON file"""
         self.load()
         self._data_list.append(item.__dict__)
         self.save()
 
-    def find_item( self, key_value, key=None):
-        """Finds the first item with the key_value in the datalist"""
+    def find_item(self, key_value, key=None):
+        """Finds the first item with the key_value in the data-list"""
         self.load()
         if key is None:
             key = self._ID_FIELD
@@ -49,8 +51,8 @@ class JsonStore():
                 return item
         return None
 
-    def find_items_list (self, key_value, key=None):
-        """Finds all the items with the key_value in the datalist"""
+    def find_items_list(self, key_value, key=None):
+        """Finds all the items with the key_value in the data-list"""
         self.load()
         if key is None:
             key = self._ID_FIELD
@@ -60,16 +62,16 @@ class JsonStore():
                 data_list_result.append(item)
         return data_list_result
 
-    def delete_json_file( self ):
+    def delete_json_file(self):
         """delete the json file"""
         if os.path.isfile(self._FILE_PATH):
             os.remove(self._FILE_PATH)
 
-    def empty_json_file( self ):
+    def empty_json_file(self):
         """removes all data from the json file"""
         self._data_list = []
         self.save()
 
-    def data_hash( self ):
+    def data_hash(self):
         """calculates the md5 hash of the file's content"""
         return hashlib.md5(self._data_list.__str__().encode()).hexdigest()
--- src/main/python/uc3m_care/vaccine_manager.py
+++ src/main/python/uc3m_care/vaccine_manager.py
@@ -3,6 +3,7 @@
 from uc3m_care.data.vaccine_patient_register import VaccinePatientRegister
 from uc3m_care.data.vaccination_appointment import VaccinationAppointment
 
+
 class VaccineManager:
     """Class for providing the methods for managing the vaccination process"""
 
@@ -11,27 +12,27 @@ class VaccineManager:
         def __init__(self):
             pass
 
-        #pylint: disable=too-many-arguments
+        # pylint: disable=too-many-arguments
         # pylint: disable=no-self-use
-        def request_vaccination_id (self, patient_id,
-                                    name_surname,
-                                    registration_type,
-                                    phone_number,
-                                    age):
-            """Register the patinent into the patients file"""
+        def request_vaccination_id(self, patient_id,
+                                   name_surname,
+                                   registration_type,
+                                   phone_number,
+                                   age):
+            """Register the patient into the patients file"""
             my_patient = VaccinePatientRegister(patient_id,
-                                                    name_surname,
-                                                    registration_type,
-                                                    phone_number,
-                                                    age)
+                                                name_surname,
+                                                registration_type,
+                                                phone_number,
+                                                age)
 
             my_patient.save_patient()
             return my_patient.patient_sys_id
 
-        def get_vaccine_date (self, input_file):
+        def get_vaccine_date(self, input_file):
             """Gets an appointment for a registered patient"""
-            my_sign= VaccinationAppointment.create_appointment_from_json_file(input_file)
-            #save the date in store_date.json
+            my_sign = VaccinationAppointment.create_appointment_from_json_file(input_file)
+            # save the date in store_date.json
             my_sign.save_appointment()
             return my_sign.date_signature
 
@@ -40,18 +41,18 @@ class VaccineManager:
             appointment = VaccinationAppointment.get_appointment_from_date_signature(date_signature)
             return appointment.register_vaccination()
 
-        #def cancel_appointment(self, input_file):
-            #añadir parámetro en vaccine appointment para comprobar cita temporal o permamnente
+        # def cancel_appointment(self, input_file):
+        # añadir parámetro en vaccine appointment para comprobar cita temporal o permamnente
 
     instance = None
 
-    def __new__ ( cls ):
+    def __new__(cls):
         if not VaccineManager.instance:
             VaccineManager.instance = VaccineManager.__VaccineManager()
         return VaccineManager.instance
 
-    def __getattr__ ( self, nombre ):
-        return getattr(self.instance, nombre)
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
 
-    def __setattr__ ( self, nombre, valor ):
-        return setattr(self.instance, nombre, valor)
+    def __setattr__(self, name, value):
+        return setattr(self.instance, name, value)
